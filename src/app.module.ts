668f2de5e52590912d8077f3f7bbdc8ea92bf8d9
ilavriv <lavriv92@gmail.com>
2016-11-11 00:02:22 +0200

    Created routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+
 import AppComponent from './screens/app/app.component';
+import IndexComponent from './screens/index/index.component';
+import MapsComponent from './screens/maps/maps.component';
+
+import mapsReducer from './screens/maps/maps.reducer';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.provideStore({
+      maps: mapsReducer
+    }),
+    RouterModule.forRoot([{
+      path: '',
+      component: IndexComponent
+    },
+    {
+      path: 'maps',
+      component: MapsComponent
+    }])
+  ],
+  declarations: [
+    AppComponent,
+    MapsComponent,
+    IndexComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/screens/maps/maps.reducer.ts
@@ -0,0 +1,17 @@
+import { ActionReducer, Action } from '@ngrx/store';
+
+interface Maps {
+  maps: Array<Object>,
+  count: number,
+  loading: Boolean
+}
+
+const mapsReducer : ActionReducer<Maps> = (state: Maps = {
+  maps: [{ id: 1, name: 'test' }],
+  count: 0,
+  loading: false
+}, action: Action) => {
+  return state;
+};
+
+export default mapsReducer;
